95367fd7b6648db3ec74df72914dd94119cf0be3
Justin Crafford <jcrafford@gmail.com>
2016-12-05 14:57:29 -0500

    A versioning strategy whereby one copies the previous version, modifies the __init__ to change version nums, and make edits to the resources as necessary.
    Uses `ct-core-api` version 2.4.0 in which Swagger UI automatically provides an authentication dialog when `security` configuration is supplied.

--- flask/app/extensions/api_ext.py
+++ flask/app/extensions/api_ext.py
@@ -14,8 +14,19 @@ authorizations = {
     }
 }
 
-api = API(version='1.0', title=u'Todo Docker', description=u'The Todo Docker Application API', authorizations=authorizations, security=['token', 'basic'])
+
+def _create_api(version):
+    return API(
+        version=version,
+        title=u'Todo Docker',
+        description=u'The Todo Docker Application API',
+        authorizations=authorizations,
+        security=['token', 'basic'])
+
+api_v1_0 = _create_api('1.0')
+api_v1_1 = _create_api('1.1')
 
 
 def init_app(app):
-    app.register_extension(api, 'api-v1')
+    app.register_extension(api_v1_0, 'api-v1.0')
+    app.register_extension(api_v1_1, 'api-v1.1')
---
+++ flask/app/modules/api_v1_1/todo_resources.py
@@ -0,0 +1,62 @@
+import logging
+
+from flask_login import current_user, login_required
+
+from ct_core_api.api.core import response
+from ct_core_api.api.core.namespace import APINamespace
+from ct_core_api.api.core.resource import APIResource
+from ct_core_api.api.core.parameters import Parameters
+from ct_core_api.core.database import db
+
+from app.schemas import todo_schemas
+from app.services.todo_service import TodoService
+
+_logger = logging.getLogger(__name__)
+
+api = APINamespace('todos', description="Todos")
+
+
+@api.route('/')
+class TodoCollectionResource(APIResource):
+    class PostTodoParameters(Parameters, todo_schemas.TodoSchema):
+        pass
+
+    @api.parameters(PostTodoParameters(), locations=['json'])
+    @api.response(todo_schemas.TodoSchema())
+    @api.response(code=response.Forbidden.code)
+    @api.response(code=response.Conflict.code)
+    def post(self, args):
+        """Create a new Todo."""
+        with api.commit_or_abort(db.session, default_error_message=u"Failed to create a new Todo."):
+            new_todo = TodoService.create(user_id=current_user.id, **args)
+            db.session.add(new_todo)
+        return new_todo
+
+
+@login_required
+@api.route('/<int:id>')
+@api.resolve_object_by_find('todo', TodoService.get)
+@api.response(code=response.NotFound.code, description=u"Todo not found.")
+class TodoResource(APIResource):
+    class PatchTodoParameters(Parameters, todo_schemas.TodoSchema):
+        pass
+
+    @api.response(todo_schemas.TodoSchema())
+    def get(self, todo):
+        """Get todo details by id."""
+        return todo
+
+    @api.parameters(PatchTodoParameters(), locations=['json'])
+    @api.response(todo_schemas.TodoSchema())
+    @api.response(code=response.Conflict.code)
+    def patch(self, args, todo):
+        """Patch todo details by id."""
+        with api.commit_or_abort(db.session, default_error_message=u'Failed to update todo details.'):
+            TodoService.update(todo, args)
+        return todo
+
+    @api.response(code=response.HTTPStatus.NO_CONTENT)
+    def delete(self, todo):
+        """Delete todo by id."""
+        with api.commit_or_abort(db.session, default_error_message=u'Failed to delete todo.'):
+            TodoService.delete(todo.id)
